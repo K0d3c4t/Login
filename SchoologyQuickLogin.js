8ea6ae047e56c09eadca5d3e8e78abf32aa29aec
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-07 07:30:54 -0500

    Update and rename Schoology Quick Login.js to SchoologyQuickLogin.js

--- SchoologyQuickLogin.js
+++ SchoologyQuickLogin.js
@@ -1,12 +1,13 @@
 function myFunction(){
 window.location.href="https://launchpad.classlink.com/cfisd" ;
+object.onload = function(){
 var userText = document.getElementById('username');
 userText.value = "foo";
 var passText = document.getElementById('password');
 passText.value = "bar";
 var submit = document.getElementById('signin');
 submit.click();};
-myFunction();
+myFunction();};
 
 
 
