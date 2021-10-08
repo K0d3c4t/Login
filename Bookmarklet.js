517d11de78a6d4b76e91b64abfeacefe04985f33
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-08 11:51:22 -0500

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,1 +1,11 @@
-javascript:var userName = foo; var passWord = bar;fetch("https://github.com/K0d3c4t/Login/blob/main/MainCode.js") .then((res) => res.text() .then((t) => eval(t)))
+javascript:
+function%20myFunction%28%29{
+var userName="foo";
+var passWord="bar";
+var%20userText=document.getElementById%28%27username%27%29%3b
+userText.value%3duserName%3b
+var%20passText%3ddocument.getElementById%28%27password%27%29%3b
+passText.value%3dpassWord%3b
+var%20submit%3ddocument.getElementById%28%27signin%27%29%3b
+submit.click%28%29%3b}%3b
+myFunction%28%29%3b
