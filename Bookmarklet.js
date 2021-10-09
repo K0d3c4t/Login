01f8575f5927214700c310a430e953df586d15cc
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-09 17:04:42 -0500

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,11 +1,2 @@
-javascript:
-function%20myFunction%28%29{
-var userName="foo";
-var passWord="bar";
-var%20userText=document.getElementById%28%27username%27%29%3b
-userText.value%3duserName%3b
-var%20passText%3ddocument.getElementById%28%27password%27%29%3b
-passText.value%3dpassWord%3b
-var%20submit%3ddocument.getElementById%28%27signin%27%29%3b
-submit.click%28%29%3b}%3b
-myFunction%28%29%3b
+javascript:function%20myFunction%28%29{var userName="foo";var passWord="bar";var%20userText=document.getElementById%28%27username%27%29%3buserText.value%3duserName%3bvar%20passText%3ddocument.getElementById%28%27password%27%29%3b
+passText.value%3dpassWord%3bvar%20submit%3ddocument.getElementById%28%27signin%27%29%3bsubmit.click%28%29%3b}%3bmyFunction%28%29%3b
